33f73de9ea9adc5049d092694d439f1f8d6c12d1
unknown <umpalump97@gmail.com>
2024-01-27 00:10:14 +0100

    przyciski cos robiom

--- meniusy/StartoweMenu.gd
+++ meniusy/StartoweMenu.gd
@@ -1,6 +1,5 @@
 extends Control
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -9,3 +8,15 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_start_pressed():
+	get_tree().change_scene_to_file("res://level.tscn")
+
+
+func _on_credits_pressed():
+	pass # Replace with function body.
+
+
+func _on_quit_pressed():
+	get_tree().root.free()
