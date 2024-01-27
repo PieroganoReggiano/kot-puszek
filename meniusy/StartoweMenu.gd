af96ec5b70606faead807edd1ef97c3b124b2369
Gabriel <gabriel.szwaczkiewicz@gmail.com>
2024-01-27 17:31:00 +0100

    menu jest super mozna klikac w nie (#17)
    * dobry jezuanaszpanie
    
    * matko dosko

--- meniusy/StartoweMenu.gd
+++ meniusy/StartoweMenu.gd
@@ -1,11 +1,27 @@
 extends Control
-
-
+var plum_player: AudioStreamPlayer2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	plum_player = get_child(2)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func plumkaj():
+	plum_player.play()
+
+func _on_start_pressed():
+	plumkaj()
+	get_tree().change_scene_to_file("res://level.tscn")
+	
+
+
+func _on_credits_pressed():
+	plumkaj()
+
+
+func _on_quit_pressed():
+	plumkaj()
+	get_tree().root.free()
