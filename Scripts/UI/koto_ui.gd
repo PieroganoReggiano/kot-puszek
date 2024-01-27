f462ac25b59cbb6ecfeaa5fc607dbdbb32fc12b8
unknown <umpalump97@gmail.com>
2024-01-27 21:48:35 +0100

    animacja butelgi

--- Scripts/UI/koto_ui.gd
+++ Scripts/UI/koto_ui.gd
@@ -1,11 +1,20 @@
 extends HBoxContainer
-
+var numersy = [11, 12, 13, 14, 15, 16]
+var pozycja_animacji = 1
+var animowane_piwo: TextureRect
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	animowane_piwo = get_child(0).get_child(0).get_child(1)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_timer_timeout():
+	animowane_piwo.set_texture(load("res://Images/UI/frames/healthbar" + str(numersy[pozycja_animacji]) + ".png"))
+	pozycja_animacji += 1
+	if pozycja_animacji == numersy.size():
+		pozycja_animacji = 0
