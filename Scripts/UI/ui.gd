85eddddb8e5d7b1e327451b28319f3b53493c7e6
Gabriel <gabriel.szwaczkiewicz@gmail.com>
2024-01-27 18:53:39 +0100

    Poprawa meniu ui (#19)

--- Scripts/UI/ui.gd
+++ Scripts/UI/ui.gd
@@ -1,16 +1,31 @@
 extends Control
-
+var plum_player: AudioStreamPlayer2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	plum_player = get_child(2)
 
+func plumkaj():
+	plum_player.play()
 
+func znikanko():
+	if get_child(1).visible:
+		get_child(1).hide()
+		get_tree().paused = false
+	else:
+		get_child(1).show()
+		get_tree().paused = true
+		
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var is_menu_visible = get_child(1).visible
 	if Input.is_action_just_released("escape"):
-		if is_menu_visible:
-			get_child(1).hide()
-		else:
-			get_child(1).show()
+		znikanko()
+
+
+func _on_return_pressed():
+	plumkaj()
+	znikanko()
+
+
+func _on_meniu_pressed():
+	plumkaj()
